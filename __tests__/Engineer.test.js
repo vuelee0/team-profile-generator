b9414c5a5a0ef76d0a7d8faee63aadd2737d9906
Vue Lee <vuelee09@gmail.com>
2022-05-10 01:36:35 -0500

    test engineer object and github properties

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require('../lib/Engineer.js');
+
+test('creats a engineer object', () => {
+    const engineer = new Engineer('Donnie', 12345, 'donnieyen@gmail.com', 'donnieyen0');
+
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+test('gets engineer github as an object', () => {
+    const engineer = new Engineer('Donnie', 12345, 'donnieyen@gmail.com', 'donnieyen0');
+
+    expect(engineer.getGithub()).toEqual(expect.any(String));
+});
+
+test('getRole overridden to return Engineer', () => {
+    const engineer = new Engineer('Donnie', 12345, 'donnieyen@gmail.com', 'donnieyen0');
+
+    expect(engineer.getRole()).toEqual('Engineer');
+});
