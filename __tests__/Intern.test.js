a399a3d902804fb0366e80cc2da58b20228f9729
Vue Lee <vuelee09@gmail.com>
2022-05-10 01:47:55 -0500

    test intern object and school properties

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
 const Intern = require('../lib/Intern.js');
+
+test('creates a intern object', () => {
+    const intern = new Intern('Donnie', 12345, 'donnieyen@gmail.com', 'schoolName');
+
+    expect(intern.schoolName).toEqual(expect.any(String));
+});
+
+test('gets intern school as an object', () => {
+    const intern = new Intern('Donnie', 12345, 'donnieyen@gmail.com', 'schoolName');
+
+    expect(intern.getSchool()).toEqual(expect.any(String));
+});
+
+test('getRole overridden to return Intern', () => {
+    const intern = new Intern('Donnie', 12345, 'donnieyen@gmail.com', 'schoolName');
+
+    expect(intern.getRole()).toEqual('Intern');
+});
