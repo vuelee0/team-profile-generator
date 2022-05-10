065f51c6f67de93153b83b1c64849f981f35e22c
Vue Lee <vuelee09@gmail.com>
2022-05-10 00:41:02 -0500

    test getname and get id methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,15 @@ test('creates a employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+test('gets employee name as an object', () => {
+    const employee = new Employee('Donnie');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('gets employee id as an object', () => {
+    const employee = new Employee('Donnie', 12345, 'donnieyen@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
