51ee560ea94bb6ac1ae92c5e39464e706ca957fb
Vue Lee <vuelee09@gmail.com>
2022-05-10 00:50:35 -0500

    test getemail and get role methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -19,3 +19,15 @@ test('gets employee id as an object', () => {
 
     expect(employee.getId()).toEqual(expect.any(Number));
 });
+
+test('gets employee email as an object', () => {
+    const employee = new Employee('Donnie', 12345, 'donnieyen@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+test('getRole should Returns Employee', () => {
+    const employee = new Employee('Donnie', 12345, 'donnieyen@gmail.com');
+
+    expect(employee.getRole()).toEqual(expect.any(String));
+});
