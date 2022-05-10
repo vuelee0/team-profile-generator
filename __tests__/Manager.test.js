962913bcf4b6fa6283b034984d53ce5a7a14973a
Vue Lee <vuelee09@gmail.com>
2022-05-10 01:18:30 -0500

    test employee object and officenumber object

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,13 @@
 const Manager = require('../lib/Manager.js');
+
+test('creates a manager object', () => {
+    const manager = new Manager('Donnie', 12345, 'donnieyen@gmail.com', 100);
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+test('getRole overridden to return Manager', () => {
+    const manager = new Manager('Donnie', 12345, 'donnieyen@gmail.com');
+
+    expect(manager.getRole()).toEqual('Manager');
+});
