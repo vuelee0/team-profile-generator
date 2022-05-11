44c35f8feb29ab5c375edde35a7219db073d1fed
Vue Lee <vuelee09@gmail.com>
2022-05-10 23:47:00 -0500

    add manager questions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const inquirer = require('inquirer');
+const generateHTML = require('./src/generatedHTML.js');
+
+// prompt to enter team manager's information
+const inputManager = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the team manager's name?",
+    },
+    {
+        type: 'input',
+        name: 'employeeId',
+        message: "What is the manager's employee ID?",
+    },
+    {
+        type: 'input',
+        name: 'emailAddress',
+        message: "What is the manager's email address?",
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the manager's office number?",
+    }
+];
